7303057af41a1c411f8367886c08814df53c53a0
Sierra Moya <smoya30@cnm.edu>
2022-04-02 16:33:51 -0600

    commit shopping list

---
+++ script.js
@@ -0,0 +1,63 @@
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+
+    form.addEventListener("submit", (event) => {
+        handleItemForm();
+    });
+}
+
+function handleItemForm (event, formRef) {
+    if(event.preventDefault) {
+        event.preventDefault();
+    }
+}
+    addItemToShoppingList();
+    formRef.reset();
+
+    return false;
+}
+
+function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+    let id = getRandomInt(0, 10000000);
+
+
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
+    console.log("Item HTML: ", itemHML);
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+    setDeleteButtonEvent(id);
+}
+function setDeleteButtonEvent(id) {
+    let deleteButton = document.getElementById("button"+id);
+    deleteButton.addEventListener("click", {} => {
+        removeListItem(id)
+
+    });
+}
+
+function createListItemHtml(itemName, itemAmount) {
+    return `<li id="item${id}">
+                ${itemName.value} - ${itemAmount.value}
+                <button type="button">Delete Item</button>
+            </li>   `;
+}
+
+function removeListItem(id) {
+    let listItem = document.getElementById("item"+id);
+    listItem.parentNode.removeChild(listItem)
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
